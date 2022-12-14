3b811f981d8318459f20d691f4c427b4e57a6356
B-Perederei <woncaru@gmail.com>
2022-12-14 21:48:39 +0200

    Change type

--- function.sql
+++ function.sql
@@ -1,7 +1,7 @@
 -- Function for getting number of releases of given game publisher
 
-DROP FUNCTION IF EXISTS publisherReleases(publisher varchar(80));
-CREATE OR REPLACE FUNCTION publisherReleases(publisher varchar(80)) RETURNS int
+DROP FUNCTION IF EXISTS publisherReleases(publisher varchar);
+CREATE OR REPLACE FUNCTION publisherReleases(publisher varchar) RETURNS int
 LANGUAGE plpgsql
 AS $$
 DECLARE
