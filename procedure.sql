e8c4b1f553ef3c1eda5fe43f74490bcaa43412af
B-Perederei <woncaru@gmail.com>
2022-12-14 21:35:42 +0200

    Added procedure

--- procedure.sql
+++ procedure.sql
@@ -1,12 +1,12 @@
--- Deleting any record in DB, where given publisher is involved
+-- Changing id and name of a given region in Region and GamePlatformRegionSales tables
 
-DROP PROCEDURE IF EXISTS deletePublisherFromDB(varchar(80));
-CREATE OR REPLACE PROCEDURE deletePublisherFromDB(IN publisher varchar(80))
+DROP PROCEDURE IF EXISTS changeRegion(old_region_id varchar, new_region_id varchar, new_region_name varchar);
+CREATE OR REPLACE PROCEDURE changeRegion(old_region_id varchar, new_region_id varchar, new_region_name varchar)
 LANGUAGE plpgsql
 AS $$
-  BEGIN
-    DELETE FROM gameplatformregionsales WHERE game_id IN (SELECT game_id FROM Game WHERE game_publisher = publisher);
-	DELETE FROM gameplatform WHERE game_id IN (SELECT game_id FROM Game WHERE game_publisher = publisher);
-	DELETE FROM game WHERE game_publisher = publisher;
-  END;
+BEGIN
+	INSERT INTO region (region_id, region_name) VALUES (new_region_id, new_region_name);
+	UPDATE gameplatformregionsales SET region_id = new_region_id WHERE region_id = old_region_id;
+	DELETE FROM region WHERE region_id = old_region_id;
+END;
 $$;
